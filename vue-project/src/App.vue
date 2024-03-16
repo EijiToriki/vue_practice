ef28b8a4b5d2021f3df3bb4380a51d4a49189e83
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-16 20:59:10 +0900

    vue Lesson2受講

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+const title = ref('Vue.js Course')
+let num = ref(1)
+const info = ref({
+  students: 1000,
+  rating: 4
+})
+const instructor = reactive({
+  nama: 'toriki',
+  age: 25,
+  sns: {
+    twitter: 'toriki',
+    insta: 'age'
+  },
+  email: ref('age@gmail.com')
+})
+console.log(info.value.students)
+console.log(instructor.email)
+
+const items = reactive([ref(1), ref(2), ref(3)])
+console.log(items[0].value)
+
+const courseInfo = {
+  sections: ref(10),
+  language: ref('japanese')
+}
+
+function increment() {
+  num.value += 1
+  console.log(num.value)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Title : {{ title }}</h1>
+  <h2>num : {{ num }}</h2>
+  <button @click="increment">button</button>
+  <h2>{{ info.students }}</h2>
+  <h2>Course Info Sections : {{ courseInfo.sections.value + 1 }}</h2>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1 {
+  color: red;
 }
 </style>

9213e36fe970bef8a59fad54abf3bd0fc5f6f528
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-21 21:30:27 +0900

    vue Lesson6 完了

---
+++ vue-project/src/Lesson6/ResetButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+const emit = defineEmits(['reset'])
+const emitReset = () =>{
+  emit('reset', 100)
+}
+</script>
+
+<template>
+  <button @click="$emit('reset', 100)">Reset</button>
+  <button @click="emitReset">Reset</button>
+</template>
---
+++ vue-project/src/Lesson6/ShowCount.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  'count': {
+    type: Number,
+    // required: true,
+    default: 100,
+    // validator: function(value){
+    //   return value === 0
+    // }
+  },
+  'bar': {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <p>count:{{ count }}</p>
+  <p>{{ bar }}</p>
+</template>

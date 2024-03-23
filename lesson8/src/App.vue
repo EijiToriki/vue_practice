5468e83ab92f433a021c9afab062581ff90317ff
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-23 22:35:06 +0900

    vue Lesson8 DOM

---
+++ lesson8/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {ref, onMounted} from 'vue'
+import CountUp from './components/CountUp.vue'
+
+const isShow = ref(true)
+const userInput = ref(null)
+onMounted(() => {
+  console.log(userInput.value)
+})
+</script>
+
+<template>
+  <div>
+    <button @click="isShow = !isShow">switch</button>
+    <CountUp v-if="isShow" />
+    <input ref="userInput" type="text" />
+  </div>
+
+</template>
+
+<style scoped>
+</style>

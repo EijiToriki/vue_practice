cb886709665d389f211db78c26e3a06186c339e4
Eiji Toriki <ageForEngi@outlook.jp>
2024-03-27 22:01:42 +0900

    vue Lesson10 フォーム

---
+++ lesson8-lesson10/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import FormSample from './lesson10/FormSample.vue';
+import CustomInput from './lesson10/CustomInput.vue'
+import { ref } from 'vue';
+
+const userInput = ref('')
+const titleName = ref('titleName')
+</script>
+
+<template>
+  <CustomInput v-model.uppercase="userInput" v-model:title-name="titleName" />
+  <p>userInput : {{ userInput }}</p>
+  <p>titleName : {{ titleName }}</p>
+  <FormSample />
+</template>
+
+<style>
+</style>
---
+++ lesson8-lesson10/src/lesson8/CountUp.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, nextTick} from 'vue'
+const count = ref(0)
+onBeforeMount(() => {
+  console.log('onBeforeMounted')
+  // debugger
+})
+
+onMounted(() => {
+  console.log('onMounted')
+  // debugger
+})
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdated')
+  // debugger
+})
+
+onUpdated(() => {
+  console.log('onUpdated')
+  // debugger
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount')
+  // debugger
+})
+
+onUnmounted(() => {
+  console.log('onUnMounted')
+  // debugger
+})
+
+const countup = () => {
+  count.value++
+  nextTick(() => {
+    console.log('nextTick')
+    debugger
+  })
+}
+</script>
+
+<template>
+  <div>
+    <p>count : {{ count }}</p>
+    <button @click="countup">+1</button>
+  </div>
+</template>

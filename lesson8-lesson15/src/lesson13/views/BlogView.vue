e850bcd368a288f6e3a63440b0f2e257a5695d31
Eiji Toriki <ageForEngi@outlook.jp>
2024-04-08 21:43:33 +0900

    Lesson13 view Router

--- lesson8-lesson15/src/lesson13/views/BlogView.vue
+++ lesson8-lesson15/src/lesson13/views/BlogView.vue
@@ -1,9 +1,23 @@
 <script setup>
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+console.log(route.params)
+
+watch(
+  () => route.params.blog_id,
+  () => {
+    console.log('watch')
+  }
+)
 
 </script>
 
 <template>
   <h2>Blog</h2>
+  <p>{{ $route }}</p>
+  <RouterLink :to="{name: 'blog', params: {blog_id: Number($route.params.blog_id) + 1}}">Next</RouterLink>
 </template>
 
 <style scoped>

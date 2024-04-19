4b42331b12d6bdbc183f3ec4ee103a70ee28f692
Eiji Toriki <ageForEngi@outlook.jp>
2024-04-19 22:08:11 +0900

    Lesson14 pinia storeの学習

---
+++ lesson8-lesson15/src/lesson13/Lesson13App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const toAbout = () => {
+  console.log('toAbout')
+  router.push({path : '/about'})
+}
+
+
+</script>
+
+<template>
+  <h1>VueRouter</h1>
+  <RouterLink to="/">Home</RouterLink> | <RouterLink to="/about">About</RouterLink> | <RouterLink :to="{name: 'profile', params: {id: 'toriki'}}">Profile</RouterLink>
+  <br>
+  <!-- <p style="margin-top: 1000px">bottom</p> -->
+  <RouterLink :to="{ path: '/', query: {lang: 'ja'}, hash: '#title' }">Home</RouterLink> | <RouterLink :to="{ name: 'about'}">About</RouterLink>
+  <br>
+  <button @click="toAbout">About</button>
+
+  <RouterView v-slot="{ Component, route }">
+    <Transition :name="route.meta.transition" mode="out-in">
+      <component :is="Component" :key="route.path" />
+    </Transition>
+  </RouterView>
+
+  
+  
+  <!-- <RouterView name="Sidebar" />
+  <RouterView name="Footer" /> -->
+</template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to{
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active{
+  transition: opacity 1s;
+}
+</style>
